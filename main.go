9b70eda4f3e48d5d906f99b570a16d5a979b0a99
DominicWuest <dominic.wuest@hotmail.com>
2024-02-22 15:45:14 +0100

    Add bug to /2

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/0", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "-1") })
 	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "-1") })
-	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "2") })
+	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "-1") })
 
 	http.ListenAndServe(":3333", nil)
 }
