76b5c32593cd9e9295db6c2e84bff32154427a65
DominicWuest <dominic.wuest@hotmail.com>
2024-02-29 16:26:06 +0100

    Add /3, /4 and /5 testing endpoints

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
 	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "-1") })
 	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "-1") })
 
+	http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "3") })
+	http.HandleFunc("/4", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "4") })
+	http.HandleFunc("/5", func(w http.ResponseWriter, r *http.Request) { io.WriteString(w, "5") })
+
 	http.ListenAndServe(":3333", nil)
 }
